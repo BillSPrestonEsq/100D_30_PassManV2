43b041579c131d9a069f472fb3dd3c0851aa0156
BillSPrestonEsq <58013891+BillSPrestonEsq@users.noreply.github.com>
2021-06-09 10:10:42 -0400

    Attempt before looking at solution

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
 from random import shuffle, choice, randint
+import json
+
+# ---------------------------------- SEARCH ------------------------------------- #
+def search_json():
+
+    global web_entry
+    website = web_entry.get()
+
+    try:
+        with open("data.json", "r") as my_file:
+            data = json.load(my_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found.")
+    else:
+        try:
+            email = data[website.lower()]["email"]
+            password = data[website.lower()]["password"]
+        except KeyError:
+            messagebox.showerror(title="Error", message="No information found for that website.")
+        else:
+            messagebox.showinfo(title=f"{website}", message=f"Email: {email}\nPassword: {password}")
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -33,24 +55,30 @@ def save_data():
     global email_entry
     global pass_entry
 
-    website = web_entry.get()
+    website = web_entry.get().lower()
     email = email_entry.get()
     password = pass_entry.get()
 
+    new_data = {
+        website: {
+            "email":email,
+            "password":password
+        }
+    }
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showerror(title="Invalid entry", message="Must fill in all fields.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
-                                                      f"\nPassword: {password}\nIs it ok to save?")
-
-        # format:
-        # Website | Email/Username | password
-        # save it to data.txt
-
-        if is_ok:
-            with open("data.txt", "a") as my_file:
-                my_file.write(f"{website} | {email} | {password}\n")
-
+        try:
+            with open("data.json", "r") as my_file:
+                data = json.load(my_file)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", "w") as my_file:
+                json.dump(new_data, my_file, indent=4)
+        else:
+            with open("data.json", "w") as my_file:
+                json.dump(data, my_file, indent=4)
+        finally:
             web_entry.delete(0, END)
             pass_entry.delete(0, END)
 
@@ -75,7 +103,7 @@ email_label.grid(row=2, column=0, sticky=E)
 pass_label = Label(text="Password: ")
 pass_label.grid(row=3, column=0, sticky=E)
 
-web_entry = Entry(width=55)
+web_entry = Entry(width=30)
 web_entry.grid(row=1, column=1, columnspan=2, sticky=W)
 web_entry.focus_set()
 
@@ -90,6 +118,9 @@ gen_pass_button.grid(row=3, column=2, sticky=W)
 
 add_button = Button(text="Add", width=47, command=save_data)
 add_button.grid(row=4, column=1, columnspan=2, sticky=W)
+
+search_button = Button(text="Search", width=18, command=search_json)
+search_button.grid(row=1, column=2, sticky=W)
 
 email_entry.insert(0, "example@example.com")
 
